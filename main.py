7dd8ae793c3469ce30aeee8bcf3865af869bc961
sabuo <sabunori1219@gmail.com>
2025-07-01 09:48:26 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py の例
+import discord
+import os # osライブラリをインポート
+
+# 環境変数からトークンを読み込む
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} としてログインしました')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
