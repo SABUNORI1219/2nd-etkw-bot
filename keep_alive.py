05d66818e5402c04cf206f22753f8880a2daa884
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-03 06:12:06 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,17 @@
+# keep_alive.py
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    server_thread = Thread(target=run)
+    server_thread.start()
