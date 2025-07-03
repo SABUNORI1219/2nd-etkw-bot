2a28d01082c0ada56a28ee873413b965a672d196
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-04 07:24:32 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    """Renderからのアクセスに応答し、サービスをアクティブに保つためのページ"""
+    return "I'm alive"
+
+def run():
+    """Flaskサーバーを起動する"""
+    # Renderが指定するホストとポートで実行
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    """Webサーバーを別スレッドで起動する"""
+    server_thread = Thread(target=run)
+    server_thread.start()
