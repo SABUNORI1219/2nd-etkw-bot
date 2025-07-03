13a767ae97605ed583bf279a78b739098119a17e
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-03 14:07:46 +0900

    Update and rename config to config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Wynncraft関連の定数
+GUILD_NAME = "Empire of TKW"
+GUILD_API_URL = f"https://nori.fish/api/guild/{GUILD_NAME.replace(' ', '%20')}"
+PLAYER_API_URL = "https://api.wynncraft.com/v3/player/{}"
+RAID_TYPES = ["tna", "tcc", "nol", "nog"]
+
+# Discord関連の定数
+# 例: Embedメッセージの色などを定義
+EMBED_COLOR_GOLD = 0xFFD700
+EMBED_COLOR_GREEN = 0x00FF00
+EMBED_COLOR_BLUE = 0x0000FF
