acd9e54ba5065ea379a1d9def608f16098dd8cf4
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-04 14:34:55 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,18 @@ import os
 
 # Wynncraft API関連
 GUILD_NAME = "Empire of TKW"
-# ギルド名にスペースが含まれる場合、URLエンコード(%20)する
-GUILD_API_URL = f"https://nori.fish/api/guild/{GUILD_NAME.replace(' ', '%20')}"
-PLAYER_API_URL = "https://api.wynncraft.com/v3/player/{}"
+NORI_GUILD_API_URL = f"https://nori.fish/api/guild/{GUILD_NAME.replace(' ', '%20')}"
+NORI_PLAYER_API_URL = "https://nori.fish/api/player/{}" # プレイヤー情報用のAPIを追加
+WYNN_PLAYER_API_URL = "https://api.wynncraft.com/v3/player/{}"
 
 # 追跡対象のレイド名 (APIで使われる内部名)
 RAID_TYPES = ["tna", "tcc", "nol", "nog"]
 
 # Discord関連 (環境変数から読み込み)
-# .envファイルやRenderの環境変数に設定してください
 GUILD_ID_INT = int(os.getenv('GUILD_ID', 0))
 TRACKING_CHANNEL_ID = int(os.getenv('NOTIFICATION_CHANNEL_ID', 0))
+
+# Embed用の色設定
+EMBED_COLOR_GOLD = 0xFFD700
+EMBED_COLOR_GREEN = 0x00FF00
+EMBED_COLOR_BLUE = 0x0000FF
