d518623beb25f1a8880a7d3ed9d37a582e5af425
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-18 01:50:48 +0900

    Update territory_cog.py

--- cogs/territory_cog.py
+++ cogs/territory_cog.py
@@ -99,7 +99,7 @@ class Territory(commands.GroupCog, name="territory"):
         guild_name = live_data['guild']['name']
         guild_prefix = live_data['guild']['prefix']
         embed.add_field(name="Guild", value=f"[{guild_prefix}] {guild_name}", inline=False)
-        embed.add_field(name="Have Held for", value=f"`{held_for}`", inline=False)
+        embed.add_field(name="Time Held for", value=f"`{held_for}`", inline=False)
         embed.add_field(name="Production", value=production_text, inline=False)
         embed.add_field(name="Original Conns", value=f"{conns_count} Conns", inline=False)
         embed.set_footer(text="Territory Status | Minister Chikuwa")
