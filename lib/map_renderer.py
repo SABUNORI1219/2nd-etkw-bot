f95d88720f36604f6502812d08d64ddab635b704
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-18 13:48:25 +0900

    Update map_renderer.py

--- lib/map_renderer.py
+++ lib/map_renderer.py
@@ -105,8 +105,7 @@ class MapRenderer:
                 draw.text(((x_min + x_max)/2, (y_min + y_max)/2), prefix, font=scaled_font, fill=color_rgb, anchor="mm", stroke_width=2, stroke_fill="black")
 
     def create_territory_map(self, territory_data: dict, territories_to_render: dict, guild_color_map: dict) -> tuple[discord.File | None, discord.Embed | None]:
-    if not territories_to_render:
-        return None, None
+    if not territories_to_render: return None, None
 
     try:
         # 地図のコピーと初期化
