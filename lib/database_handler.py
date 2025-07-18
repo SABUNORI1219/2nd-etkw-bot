f53bf14730a6eec0edf3d2d100436d5edb7ec7cc
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-19 00:24:51 +0900

    Update database_handler.py

--- lib/database_handler.py
+++ lib/database_handler.py
@@ -59,7 +59,11 @@ def set_setting(key: str, value: str):
         return
     try:
         with conn.cursor() as cur:
-            cur.execute(sql, (key, value))
+            cur.execute(
+            CREATE TABLE IF NOT EXISTS bot_settings (
+                key TEXT PRIMARY KEY,
+                value TEXT NOT NULL
+            );
             conn.commit()
     except Exception as e:
         logger.error(f"[DB Handler] 設定の保存に失敗: {e}")
