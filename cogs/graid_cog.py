cabb7c6c993e03b46ad81caad016f364e9022de8
SABUNORI1219 <sabunori1219@gmail.com>
2025-07-19 07:55:15 +0900

    Update graid_cog.py

--- cogs/graid_cog.py
+++ cogs/graid_cog.py
@@ -8,13 +8,13 @@ from lib.database_handler import set_setting
 logger = logging.getLogger(__name__)
 
 @app_commands.checks.has_permissions(administrator=True) # このグループのコマンドは管理者のみ
-class TrackerCog(commands.GroupCog, group_name="raidtracker", description="レイドトラッカー関連のコマンド"):
+class TrackerCog(commands.GroupCog, group_name="graid", description="ギルドレイド関連"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         logger.info(f"--- [Cog] {self.__class__.__name__} が読み込まれました。")
 
-    @app_commands.command(name="channel", description="ギルドレイドの通知を送信するチャンネルを設定します。")
-    @app_commands.describe(channel="通知を送信するテキストチャンネル")
+    @app_commands.command(name="channel", description="ギルドレイドの通知を送信するチャンネルを設定")
+    @app_commands.describe(channel="通知を送信するチャンネル")
     async def set_channel(self, interaction: discord.Interaction, channel: discord.TextChannel):
         set_setting("raid_notification_channel", str(channel.id))
         await interaction.response.send_message(
