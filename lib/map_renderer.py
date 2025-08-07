e5b5f8858c02ce2b71f9c6218ac562442f496848
SABUNORI1219 <sabunori1219@gmail.com>
2025-08-07 18:36:32 +0900

    Update map_renderer.py

--- lib/map_renderer.py
+++ lib/map_renderer.py
@@ -97,30 +97,96 @@ class MapRenderer:
                 "resources": self.local_territories[t].get("resources", {})
             })
         # Conn含むExt多い順→Conn多い順→HQバフ多い順→取得時刻古い順
-        hq_stats.sort(key=lambda x: (-x["ext"], -x["conn"], -x["hq_buff"], x["acquired"]))
+        hq_stats.sort(key=lambda x: (-(x["conn"] + x["ext"]), -x["conn"], -x["ext"], -x["hq_buff"], x["acquired"]))
         top5 = hq_stats[:5]
         total_res = self._sum_resources(owned_territories)
 
-        # Step2: Conn最大グループ
-        max_conn = max(x["conn"] for x in top5)
-        conn_group = [x for x in top5 if x["conn"] == max_conn]
+        # Connが2個以上多いもの優先ロジック
+        ext_max = max(x["ext"] for x in top5)
+        conn_max = max(x["conn"] for x in top5)
+        for cand in top5:
+            # Ext最大グループ内でなくとも、Connが2以上多ければ優先
+            if cand["conn"] - conn_max >= 2:
+                return cand["name"], hq_stats, top5, total_res
 
-        # Step3: その中でExt<20なもの
-        conn_group_ext_lt20 = [x for x in conn_group if x["ext"] < 20]
+        # (A) Conn含むExt同数複数→Conn多い方
+        max_conn_ext = max(x["conn"] + x["ext"] for x in top5)
+        conn_ext_tops = [x for x in top5 if x["conn"] + x["ext"] == max_conn_ext]
+        if len(conn_ext_tops) > 1:
+            conn_max_in_group = max(x["conn"] for x in conn_ext_tops)
+            conn_maxs = [x for x in conn_ext_tops if x["conn"] == conn_max_in_group]
+            if len(conn_maxs) == 1:
+                return conn_maxs[0]["name"], hq_stats, top5, total_res
+        else:
+            conn_maxs = conn_ext_tops
 
-        # Step4: その中に街があれば街
-        city = next((x for x in conn_group_ext_lt20 if x["is_city"]), None)
-        if city:
-            return city["name"], hq_stats, top5, total_res
+        # (B) 所持領地6個以下→Time Held最長
+        if len(owned_territories) <= 6:
+            oldest = min(top5, key=lambda x: x["acquired"] or "9999")
+            return oldest["name"], hq_stats, top5, total_res
 
-        # Step5: そうでなければExt最大グループ（同値ならHQバフ最大）
-        if conn_group:
-            max_ext = max(x["ext"] for x in conn_group)
-            ext_group = [x for x in conn_group if x["ext"] == max_ext]
-            hq_max = max(ext_group, key=lambda x: x["hq_buff"])
-            return hq_max["name"], hq_stats, top5, total_res
+        # (C) Conn同数&Ext<20で街領地あればそれを優先
+        if len(conn_maxs) > 1 and conn_maxs[0]["conn"] == conn_maxs[-1]["conn"]:
+            if conn_maxs[0]["ext"] < 20:
+                city = next((x for x in conn_maxs if x["is_city"]), None)
+                if city:
+                    return city["name"], hq_stats, top5, total_res
+            else:
+                # Ext20以上はConn含むExtが最大の方
+                conn_ext_max = max(x["conn"] + x["ext"] for x in conn_maxs)
+                ext_maxs = [x for x in conn_maxs if (x["conn"] + x["ext"]) == conn_ext_max]
+                if len(ext_maxs) == 1:
+                    return ext_maxs[0]["name"], hq_stats, top5, total_res
+                else:
+                    hq_max = max(ext_maxs, key=lambda x: x["hq_buff"])
+                    return hq_max["name"], hq_stats, top5, total_res
 
-        # ここまで来たら念のためtop5の最初を返しておく
+        # (D) Ext,Conn同値→資源判定
+        if len(conn_maxs) > 1 and all(x["conn"] == conn_maxs[0]["conn"] and x["ext"] == conn_maxs[0]["ext"] for x in conn_maxs):
+            res_priority = ["crops", "ore", "wood", "fish"]
+            # 一番生産量のない資源
+            min_val = float("inf")
+            min_type = None
+            for rtype in res_priority:
+                val = total_res.get(rtype, 0)
+                if val > 0 and val < min_val:
+                    min_val = val
+                    min_type = rtype
+            # その資源を生産する領地のうち、HQ候補地に一番近い
+            if min_type:
+                from math import inf
+                def trading_route_distance(start, goals):
+                    queue = collections.deque()
+                    visited = set()
+                    queue.append((start, 0))
+                    while queue:
+                        current, dist = queue.popleft()
+                        if current in visited:
+                            continue
+                        visited.add(current)
+                        if current in goals:
+                            return dist
+                        neighbors = self.local_territories.get(current, {}).get("Trading Routes", [])
+                        neighbors = [n for n in neighbors if n in owned_territories]
+                        queue.extend((n, dist+1) for n in neighbors)
+                    return inf
+                res_territories = [x for x in hq_stats if int(x["resources"].get(min_type, "0")) > 0]
+                res_names = [x["name"] for x in res_territories]
+                min_dist = inf
+                best_cand = conn_maxs[0]
+                for cand in conn_maxs:
+                    d = trading_route_distance(cand["name"], set(res_names))
+                    if d < min_dist:
+                        min_dist = d
+                        best_cand = cand
+                    elif d == min_dist:
+                        cand_res = int(self.local_territories[cand["name"]].get("resources", {}).get(min_type, "0"))
+                        best_res = int(self.local_territories[best_cand["name"]].get("resources", {}).get(min_type, "0"))
+                        if cand_res > best_res:
+                            best_cand = cand
+                return best_cand["name"], hq_stats, top5, total_res
+
+        # どれも該当しない場合はtop5の最初
         return top5[0]["name"], hq_stats, top5, total_res
 
     def _draw_trading_and_territories(self, map_to_draw_on, box, is_zoomed, territory_data, guild_color_map, hq_territories=None):
