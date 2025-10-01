9b5084ed714e24394f87bd34799fcc985d1d8bd3
SABUNORI1219 <sabunori1219@gmail.com>
2025-10-01 09:21:30 +0900

    Update subproc_map_worker.py

--- lib/subproc_map_worker.py
+++ lib/subproc_map_worker.py
@@ -5,26 +5,42 @@ import pickle
 from lib.map_renderer import MapRenderer
 
 def main():
-    # 標準入力からパラメータ受け取り
     params = pickle.load(sys.stdin.buffer)
     renderer = MapRenderer()
-    # 画像生成
-    file, embed = renderer.create_territory_map(
-        territory_data=params['territory_data'],
-        territories_to_render=params['territories_to_render'],
-        guild_color_map=params['guild_color_map'],
-        owned_territories_map=params['owned_territories_map']
-    )
-    # 結果をファイルバイト列で返す（Discord.Fileはファイル or BytesIOなのでバイト列送信）
-    map_bytes = None
-    if file is not None:
-        file.fp.seek(0)
-        map_bytes = file.fp.read()
-        file.close()
-    pickle.dump({
-        'map_bytes': map_bytes,
-        'embed_dict': embed.to_dict() if embed else None,
-    }, sys.stdout.buffer)
+    mode = params.get("mode", "map")
+
+    result = {}
+    if mode == "map":
+        file, embed = renderer.create_territory_map(
+            territory_data=params['territory_data'],
+            territories_to_render=params['territories_to_render'],
+            guild_color_map=params['guild_color_map'],
+            owned_territories_map=params['owned_territories_map']
+        )
+        map_bytes = None
+        if file is not None:
+            file.fp.seek(0)
+            map_bytes = file.fp.read()
+            file.close()
+        result = {
+            'map_bytes': map_bytes,
+            'embed_dict': embed.to_dict() if embed else None,
+        }
+    elif mode == "single":
+        image_bytes = renderer.create_single_territory_image(
+            params['territory'],
+            params['territory_data'],
+            params['guild_color_map'],
+        )
+        img_bytes = None
+        if image_bytes:
+            image_bytes.seek(0)
+            img_bytes = image_bytes.read()
+            image_bytes.close()
+        result = {
+            'image_bytes': img_bytes,
+        }
+    pickle.dump(result, sys.stdout.buffer)
 
 if __name__ == "__main__":
     main()
