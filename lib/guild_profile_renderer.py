44959acfab197c6613bf80f07f344aa35d001d1b
SABUNORI1219 <sabunori1219@gmail.com>
2025-10-24 22:00:41 +0900

    Adjust vertical positions for layout elements

--- lib/guild_profile_renderer.py
+++ lib/guild_profile_renderer.py
@@ -487,13 +487,13 @@ def create_guild_image(guild_data: Dict[str, Any], banner_renderer, max_width: i
     left_icon_x = margin
 
     # 2本目横線
-    line_y2 = info_y + 45
+    line_y2 = stat_y + 25
 
     # Created/Season
     info_y = line_y2 + icon_size + 12
 
     # 3本目横線
-    line_y3 = line_y2 + 85
+    line_y3 = line_y2 + 65
 
     # オンラインメンバー部の高さ動的計算
     role_order = ["CHIEF", "STRATEGIST", "CAPTAIN", "RECRUITER", "RECRUIT"]
