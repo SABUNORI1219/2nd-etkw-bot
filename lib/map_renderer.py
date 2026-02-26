052e2b782d867c74b7b4c59b0a511118889112f8
SABUNORI1219 <sabunori1219@gmail.com>
2026-02-26 23:20:54 +0900

    map scale test v5

--- lib/map_renderer.py
+++ lib/map_renderer.py
@@ -24,7 +24,7 @@ class MapRenderer:
             raise
 
     def _coord_to_pixel(self, x, z):
-        return x + 2550, z + 6625
+        return x + 2560, z + 6630
 
     def _hex_to_rgb(self, hex_color: str) -> tuple:
         hex_color = hex_color.lstrip('#')
